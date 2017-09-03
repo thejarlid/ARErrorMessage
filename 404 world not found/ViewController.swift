a8db5cbb11945b18c590b9026b431c435f06770d
Dilraj <dilrajsinghdevgun@hotmail.co.uk>
2017-09-03 13:18:07 -0700

    finished playful project where we can spam the world with windows errors

--- 404 world not found/ViewController.swift
+++ 404 world not found/ViewController.swift
@@ -78,28 +78,51 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
     
-    @IBAction func didTapOnScreen(_ sender: Any) {
-        
-        let imagePlane = SCNPlane(width: 0.05, height: 0.02)
-        
-        let errorMaterial = SCNMaterial()
-        errorMaterial .diffuse.contents = UIImage(named: "Error")
-        errorMaterial.isDoubleSided = true
-        
-        
-        imagePlane.materials = [errorMaterial]
-        
-        let planenode = SCNNode(geometry: imagePlane)
-        self.sceneView.scene.rootNode.addChildNode(planenode)
-        
-        if let currentFrame = sceneView.session.currentFrame {
-            // Create a transform with a translation of 0.2 meters in front of the camera
-            var translation = matrix_identity_float4x4
-            translation.columns.3.z = -0.2
-            let rotation = GLKMatrix4RotateZ(GLKMatrix4Identity, Float(Double.pi/2))
-            let transform = simd_mul(currentFrame.camera.transform, simd_mul(rotation, translation))
-            planenode.simdTransform = transform
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        DispatchQueue.main.async {
+            if self.isTouching {
+                let imagePlane = SCNPlane(width: 0.05, height: 0.02)
+                
+                let errorMaterial = SCNMaterial()
+                errorMaterial .diffuse.contents = UIImage(named: "Error")
+                errorMaterial.isDoubleSided = true
+                
+                
+                imagePlane.materials = [errorMaterial]
+                
+                let planenode = SCNNode(geometry: imagePlane)
+                self.sceneView.scene.rootNode.addChildNode(planenode)
+                
+                if let currentFrame = self.sceneView.session.currentFrame {
+                    // Create a transform with a translation of 0.2 meters in front of the camera
+                    var translation = matrix_identity_float4x4
+                    translation.columns.3.z = -0.2
+                    let rot = GLKMatrix4RotateZ(GLKMatrix4Identity, Float(Double.pi/2))
+                    let rotation = self.convertGLKMatrix4Tosimd_float4x4(matrix: rot)
+                    let transform = simd_mul(currentFrame.camera.transform, simd_mul(rotation, translation))
+                    planenode.simdTransform = transform
+                }
+            }
         }
-        //planenode.eulerAngles = SCNVector3(x: 0, y: 0, z: GLKMathDegreesToRadians(-90))
+    }
+    
+    func convertGLKMatrix4Tosimd_float4x4(matrix: GLKMatrix4) -> float4x4{
+        return float4x4(float4(matrix.m00,matrix.m01,matrix.m02,matrix.m03),
+                        float4( matrix.m10,matrix.m11,matrix.m12,matrix.m13 ),
+                        float4( matrix.m20,matrix.m21,matrix.m22,matrix.m23 ),
+                        float4( matrix.m30,matrix.m31,matrix.m32,matrix.m33 ))
+    }
+    
+    var isTouching = false
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isTouching = true
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isTouching = touches.count - 1 > 0
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isTouching = touches.count - 1 > 0
     }
 }
